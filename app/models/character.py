411b9a8cdeb47b9b092bd2156c9547b648949a3e
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-07-20 19:43:49 +0300

    Added patch and delete requests

--- app/models/character.py
+++ app/models/character.py
@@ -4,9 +4,20 @@ from datetime import date
 
 class Character(BaseModel):
     name: str
+    surname: Optional[str] = None
     nickname: Optional[str] = None
     bio: Optional[str] = None
     date_of_birth: date
     date_of_death: Optional[date] = None
     origin: Optional[str] = None
-    image_url: HttpUrl
+    image_url: Optional[HttpUrl] = None
+
+class CharacterUpdate(BaseModel): 
+   name: Optional[str]
+   surname: Optional[str]
+   nickname: Optional[str] 
+   bio: Optional[str] 
+   date_of_birth: Optional[date]
+   date_of_death: Optional[date] = None
+   origin: Optional[str] 
+   image_url: Optional[HttpUrl]
--- app/routers/character.py
+++ app/routers/character.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from uuid import UUID
 from fastapi.encoders import jsonable_encoder
 from app.core.supabase_client import supabase
-from app.models.character import Character
+from app.models.character import Character, CharacterUpdate
 
 router = APIRouter()
 
 @router.get('/', response_model=list[Character])
 def get_characters():
     res = supabase.table('character').select('*').execute()
-    print("DEBUG DATA:", res)
     return res.data
 
 @router.post('/')
@@ -16,3 +16,32 @@ def create_character(character: Character):
     character_data = jsonable_encoder(character)
     res = supabase.table('character').insert(character_data).execute()
     return res.data[0]
+
+@router.patch('/{id}')
+def update_character(id: UUID, update_data: CharacterUpdate):
+    update_data = jsonable_encoder(update_data)
+
+    if not update_data:
+        raise HTTPException(status_code=400, detail="No data to update")
+    
+    resposne = (
+        supabase
+        .table('character')
+        .update(update_data)
+        .eq('id', id)
+        .execute()
+    )
+
+    if resposne.data:
+        return resposne.data[0]
+    else:
+        raise HTTPException(status_code=404, detail="Character not found")
+    
+@router.delete('/{id}')
+def delete_character(id: UUID):
+    res = supabase.table('character').delete().eq('id', id).execute()
+
+    if res.data and len(res.data) > 0:
+        return {'message': 'Character deleted successfully!'}
+    else:
+        raise HTTPException(status_code=404, detail="Character not found")
