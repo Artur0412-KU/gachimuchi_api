d29acc316e1e6f89c275c35bdcb86c16aa5e66fa
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-07-26 19:51:32 +0300

    -

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers import character
 
+# run: uvicorn app.main:app --reload
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False}, debug=True)
 app.include_router(character.router, prefix='/chatacter', tags=['Characters'])
