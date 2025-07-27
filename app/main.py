2b636d477428b7e37b52ecc4e40c094e92855ff9
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-07-27 22:45:59 +0300

    Added request for media

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.routers import character
 from app.routers import quote
+from app.routers import media
 
 # run: uvicorn app.main:app --reload
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False}, debug=True)
 app.include_router(character.router, prefix='/chatacter', tags=['Characters'])
 app.include_router(quote.router, prefix='/quote', tags=['Quotes'])
+app.include_router(media.router, prefix='/media', tags=['Media'])
