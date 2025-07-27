400862f83ed403d1bec1fa20b2820e1df5d7a851
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-07-27 21:37:13 +0300

    Added delete request and modified patch post

--- app/routers/quote.py
+++ app/routers/quote.py
@@ -11,8 +11,12 @@ def get_quotes():
     res = supabase.table('quote').select('*').execute()
     return res.data
 
-@router.post('/')
-def add_quote(quote: Quote):
+@router.post('/{character_id}')
+def add_quote(character_id: UUID, quote: UpdateQuote):
+    character_data = supabase.table('character').select('id').eq('id', str(character_id)).execute()
+    if not character_data:
+        raise HTTPException(status_code=400, detail="Character not found")
+    
     quote_data = jsonable_encoder(quote)
     res = supabase.table('quote').insert(quote_data).execute()
     return res.data
@@ -29,5 +33,15 @@ def update_quote(id: UUID, update_data: UpdateQuote):
 
     if res.data:
         return res.data[0]
+    else:
+        raise HTTPException(status_code=404, detail="Quote not found")
+    
+
+@router.delete('/{id}')
+def delete_quote(id: UUID):
+    res = supabase.table('quote').delete().eq('id', id).execute()
+
+    if res.data:
+        return {'message': 'quote deleted successfully'}
     else:
         raise HTTPException(status_code=404, detail="Quote not found")
