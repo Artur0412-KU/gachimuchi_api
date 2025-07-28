ceaab0998868de05b4afd0cc0498de246963487b
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-07-28 19:20:18 +0300

    Added start script for deploying to render.com

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ from app.routers import quote
 from app.routers import media
 
 # run: uvicorn app.main:app --reload
-app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False}, debug=True)
+app = FastAPI(
+    title="Gachimuchi API",
+    description="A serious place for ♂real men♂. This API lets you add, search, and store iconic quotes and media from the most powerful warriors of the male universe.",
+    swagger_ui_parameters={"syntaxHighlight": False}, 
+    debug=True
+)
 app.include_router(character.router, prefix='/chatacter', tags=['Characters'])
 app.include_router(quote.router, prefix='/quote', tags=['Quotes'])
 app.include_router(media.router, prefix='/media', tags=['Media'])
